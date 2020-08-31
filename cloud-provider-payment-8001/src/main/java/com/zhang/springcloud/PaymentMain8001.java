d0de3c2a2eefecb58dd22338b41934a936a4bc21
zhang1990zxc <1018736264@qq.com>
2020-08-31 16:29:57 +0800

    工程创建

---
+++ cloud-provider-payment-8001/src/main/java/com/zhang/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.zhang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author zhang
+ * @Description 启动类
+ * @Date 2020-08-31 16:19
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
